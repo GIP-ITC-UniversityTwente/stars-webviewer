f420c16882f7262dac3b2f03272418e360251a82
clintcabanero <clint@bigleafmobile.com>
2017-06-13 18:30:26 -0700

    adds placeholder leaflet map

---
+++ stars-webapp/src/app/app-configuration.ts
@@ -0,0 +1,9 @@
+export class AppConfiguration {
+
+  /**
+   * configuration related to Leaflet map
+   */
+  static baseMapAccessToken: string  = "pk.eyJ1Ijoic3BhdGlhbGRldiIsImEiOiJKRGYyYUlRIn0.PuYcbpuC38WO6D1r7xdMdA"; // SpatialDev access token
+  static mapCenterLat: number = 16.878147;
+  static mapCenterLng: number = 15.644531;
+}
--- stars-webapp/src/app/study-area/study-area.component.ts
+++ stars-webapp/src/app/study-area/study-area.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AppConfiguration } from '../app-configuration';
+
+// reference to leaflet
+declare let L: any;
 
 @Component({
   selector: 'app-study-area',
@@ -10,6 +14,8 @@ export class StudyAreaComponent implements OnInit {
   /**
     Properties
    */
+
+  map: any;
 
   selectedStudyArea = "";
   studyAreas = [
@@ -46,5 +52,38 @@ export class StudyAreaComponent implements OnInit {
    */
   constructor() { }
 
-  ngOnInit() { }
+  ngOnInit() {
+
+    this.initializeMap()
+  }
+
+  /**
+   * For initializing the map
+   */
+  initializeMap() {
+
+    // map center and zoom
+    this.map = L.map('map').setView([AppConfiguration.mapCenterLng, AppConfiguration.mapCenterLat], 3);
+
+    // define aerial
+    let osmAttr = '&copy; <a target="_blank" href="http://openstreetmap.org">OpenStreetMap</a>';
+    let tileLayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + AppConfiguration.baseMapAccessToken, {
+      id: 'mapbox.satellite',
+      attribution: osmAttr
+    });
+
+    /*
+    // define aerial
+    let attribution = 'i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community';
+    let mapLink = '<a target="_blank" href="http://www.esri.com/">Esri</a>';
+    let tileLayer = L.tileLayer(
+      'http://server.arcgisonline.com/arcgis/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+        attribution: '&copy; ' + mapLink + ', ' + attribution,
+        maxZoom: 18,
+      });
+    */
+
+    // add layer
+    tileLayer.addTo(this.map);
+  }
 }
