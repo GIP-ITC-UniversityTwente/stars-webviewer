a1f0089ec9f394469cf05bf4b93c89b9e5045534
clintcabanero <clint@bigleafmobile.com>
2017-07-25 10:16:09 -0700

    repairs StudyAreaComponent spec

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,25 +1,58 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let debugHeaderTitle: DebugElement;
+  let elementHeaderTitle: HTMLElement;
+  let debugLogo: DebugElement;
+  let elementLogo: HTMLElement;
 
+  // async beforeEach is called first
   beforeEach(async(() => {
+
+    // call configureTestingModule within that TestBed can reset itself to a base state before each test runs
     TestBed.configureTestingModule({
+
+      // declares the test component
       declarations: [ HeaderComponent ]
     })
-    .compileComponents();
+    .compileComponents(); // compiles external template (templateUrl) and styles (styleUrls)
   }));
 
+  // synchronous beforeEach
   beforeEach(() => {
+
+    // a fixture creates a handle on the test environment surrounding the created component
     fixture = TestBed.createComponent(HeaderComponent);
+
+    // create an instance of the component-under-test
     component = fixture.componentInstance;
-    fixture.detectChanges();
+
+    // use DebugElement to query the DOM then fetch the associated DOM element
+    debugHeaderTitle = fixture.debugElement.query(By.css('.headerTitle'));
+    elementHeaderTitle = debugHeaderTitle.nativeElement;
+
+    debugLogo = fixture.debugElement.query(By.css('.logoImage'));
+    elementLogo = debugLogo.nativeElement;
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a header title that is bound to the title field', () => {
+    fixture.detectChanges();
+    expect(elementHeaderTitle.textContent).toContain(component.title);
+  });
+
+  it('should have an updated header title when the title field is mutated', () => {
+    component.title = 'CSSL';
+    fixture.detectChanges();
+    expect(elementHeaderTitle.textContent).toContain(component.title);
   });
 });
--- src/app/study-area/study-area.component.spec.ts
+++ src/app/study-area/study-area.component.spec.ts
@@ -1,14 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdMenuModule, MdIconModule, MdButtonModule, MdSelectModule, MdCheckboxModule, MdCardModule, MdTooltipModule } from '@angular/material';
 
 import { StudyAreaComponent } from './study-area.component';
+import { StarsAPIService } from '../services/stars-api.service';
+import { UserSelectionService } from '../services/user-selection.service';
 
 describe('StudyAreaComponent', () => {
   let component: StudyAreaComponent;
   let fixture: ComponentFixture<StudyAreaComponent>;
+  let userSelectionServiceStub;
+  let starsAPIServiceStub;
 
   beforeEach(async(() => {
+
+    userSelectionServiceStub = {};
+    starsAPIServiceStub = {};
+
     TestBed.configureTestingModule({
-      declarations: [ StudyAreaComponent ]
+      imports: [ BrowserModule, FormsModule, HttpModule, BrowserAnimationsModule, MdSelectModule, MdCheckboxModule, MdTooltipModule ],
+      declarations: [ StudyAreaComponent ],
+      providers: [
+        { provide: UserSelectionService, useValue: userSelectionServiceStub }, // stub of service
+        StarsAPIService // actual service
+      ]
     })
     .compileComponents();
   }));
@@ -22,4 +42,10 @@ describe('StudyAreaComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+
+  it('stub object and injected UserSelectionService should not be the same', () => {
+    expect(UserSelectionService === userSelectionServiceStub).toBe(false);
+  });
+
 });
