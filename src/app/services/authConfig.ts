adfc3d716232140cfb1bf786db24f8d4205a96c9
clintcabanero <clint@bigleafmobile.com>
2017-09-26 18:51:50 -0700

    updates auth0 config

--- src/app/services/authConfig.ts
+++ src/app/services/authConfig.ts
@@ -7,6 +7,6 @@ interface AuthConfig {
 export const AUTH_CONFIG: AuthConfig = {
   clientID: '2yX67jth2pOwXEBN3vZWRPvlduh61qzh',   // see dashboard > clients >  app > settings
   domain: 'stars-itc.eu.auth0.com',               // see dashboard > clients >  app > settings
-  //callbackURL: 'https://stars.itc.nl'             // prod
-  callbackURL: 'http://localhost:4200/callback' // development
+  callbackURL: 'https://stars.itc.nl'             // prod
+  //callbackURL: 'http://localhost:4200/callback' // development
 };
